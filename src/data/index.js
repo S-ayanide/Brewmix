bbf02110db6511cd9ced04266cd5673508ad0945
Sayan Mondal <sayan@chaosnative.com>
2022-02-28 10:22:37 +0530

    Updating coding and bollywood lofi track
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- src/data/index.js
+++ src/data/index.js
@@ -1,31 +1,32 @@
-export const data = [{
+export const data = [
+  {
     id: 1,
-    mood: 'reading',
-    lofi: 'https://www.youtube.com/embed/5qap5aO4i9A'
-},
-{
+    mood: "reading",
+    lofi: "https://www.youtube.com/embed/5qap5aO4i9A",
+  },
+  {
     id: 2,
-    mood: 'coding',
-    lofi: 'https://www.youtube.com/embed/PY8f1Z3nARo'
-},
-{
+    mood: "coding",
+    lofi: "https://www.youtube.com/embed/9AdPU89AuC0",
+  },
+  {
     id: 3,
-    mood: 'bollywood',
-    lofi: 'https://www.youtube.com/embed/Z50m2J7BwU8'
-},
-{
+    mood: "bollywood",
+    lofi: "https://www.youtube.com/embed/2q8yURJoIyI",
+  },
+  {
     id: 4,
-    mood: 'night time',
-    lofi: 'https://www.youtube.com/embed/DWcJFNfaw9c'
-},
-{
+    mood: "night time",
+    lofi: "https://www.youtube.com/embed/DWcJFNfaw9c",
+  },
+  {
     id: 5,
-    mood: 'jazzy',
-    lofi: 'https://www.youtube.com/embed/5yx6BWlEVcY'
-},
-{
+    mood: "jazzy",
+    lofi: "https://www.youtube.com/embed/5yx6BWlEVcY",
+  },
+  {
     id: 6,
-    mood: 'early morning',
-    lofi: 'https://www.youtube.com/embed/XDh0JcxrbPM'
-},
-]
+    mood: "early morning",
+    lofi: "https://www.youtube.com/embed/XDh0JcxrbPM",
+  },
+];
